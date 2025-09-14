7bbf0c8e91b79d9a37e54931225f9752b9db7a3f
Sam Korn <korn94sam@gmail.com>
2025-09-14 16:10:41 -0600

    0005: get O(n^2) solution for palindromic substring

--- 0005_longest_palindromic_string/test_solution.py
+++ 0005_longest_palindromic_string/test_solution.py
@@ -23,6 +23,19 @@ class Solution:
         else:
             return False
 
+    def is_palindrome2(self, s: str) -> bool:
+        start = 0
+        end = len(s)-1
+
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+
+        return True
+
+
     def longestPalindrome_bruteforce(self, s: str) -> str:
         max_len = len(s)
         longest_palindrome_str = ""
@@ -63,6 +76,57 @@ class Solution:
         return ""
 
 
+    def longestPalindrome_on2(self, s: str) -> str:
+        # palindrome length = 1 is initial longest
+        longest_pal = s[0]
+        max_len = len(s)
+
+        # use an internal function so we dont need to pass in shared variables
+        def expandFromCenter(left: int, right: int) -> (str, int):
+            """
+            if odd -> right - left = 2
+            if even -> right - left = 1
+
+            check even, 2 abba -> a -> left = a,0 , right = b,1
+            check odd, 3 abba -> a -> left = a,0 , right = b,2
+
+            """
+        
+            while left >= 0 and right < max_len:
+                if s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                else:
+                    break
+
+            # both left/right get incremented one extra, and then fails
+            # so a typical length of r-l +1 -2 = r-l-1
+            palindrome_length = right - left - 1
+            palindrome_string = s[left+1:right]
+
+
+            print(f"Found palindrome -> {palindrome_string}, with length: {palindrome_length}")
+            # return palindrome, length of palindrome
+            return palindrome_string, palindrome_length
+
+        
+        for i in range(max_len):
+            # 1,3,5,7
+            # i,i since 1char palindrome exists
+            pal_odd_str, pal_odd_len = expandFromCenter(i,i)
+            # 2, 4
+            # i,i+2 since checks equality of i and i+1 initial
+            # palidrome of length 2, 4
+            pal_even_str, pal_even_len = expandFromCenter(i,i+1)
+            print(pal_even_str, pal_odd_str)
+            current_len, current_pal = max((pal_odd_len, pal_odd_str),(pal_even_len, pal_even_str))
+
+            if current_len > len(longest_pal):
+                print(f"New longest: {longest_pal}")
+                longest_pal = current_pal
+
+        return longest_pal
+
 
 
 # Test cases
@@ -70,12 +134,13 @@ class Solution:
     ("babad", "bab"),
     ("testtset", "testtset"),
     ("1", "1"),
-
+    ("cbbd", "bb")
 ])
 def test_solution(x, expected):
     solution = Solution()
-    # assert solution.longestPalindrome_bruteforce(x) == expected
+    assert solution.longestPalindrome_bruteforce(x) == expected
     assert solution.longestPalindrome(x) == expected
+    assert solution.longestPalindrome_on2(x) == expected
 
 @pytest.mark.parametrize("x, expected", [
     ("babab", True),
@@ -88,3 +153,4 @@ def test_solution(x, expected):
 def test_solution2(x, expected):
     solution = Solution()
     assert solution.is_palindrome(x) == expected
+    assert solution.is_palindrome2(x) == expected
