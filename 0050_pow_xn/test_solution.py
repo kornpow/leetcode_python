e173e0fee596ca4c9d65e970f0835bccc5a6e7f1
Sam Korn <korn94sam@gmail.com>
2025-09-18 10:29:30 -0600

    0050: solve using recursive and memoization

--- 0050_pow_xn/test_solution.py
+++ 0050_pow_xn/test_solution.py
@@ -1,6 +1,4 @@
 import pytest
-from typing import List, Optional, Type
-
 
 # Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
@@ -35,13 +33,58 @@ class Solution:
     # recursion?
     # x^4 = x^3 * x => x^n = x ^ n-1
 
+    # x^n = [0,0,0,0,0]
+
+    # x*x*x*x
+    # x^2 = x*x
+    # x^3 = x * x^2
+    # x^4 = x^2 * x^2
+    # x^5 = x * x^4
+
+    # 4^10 -> 2^2
+    # n = 5
+    # its odd
+    # divmod(n,2)
+
     # TODO: someday reimplement using recursive binary exponentiation
     # TIME: O(log n)
     # SPACE: O(log n)
     def myPow(self, x: float, n: int) -> float:
-        if n < 0:
-            x = 1 / x
-            n = abs(n)
+        memo = {}
+
+        def power_recursive(x: float, n: int) -> float:
+            if n in memo:
+                return memo[n]
+
+            if n == 0:
+                memo[n] = 1
+                return memo[n]
+
+            if n == 1:
+                memo[n] = x
+                return memo[n]
+
+            if n < 0:
+                x = 1 / x
+                n = abs(n)
+
+            is_even = bool(n % 2) == 0
+
+            if is_even:
+                # 2^4 = 2^2 * 2^2
+                result = power_recursive(x, n // 2) * power_recursive(x, n // 2)
+                memo[n] = result
+                return memo[n]
+            else:
+                # 2^3 = 2^2 * 2^1
+                divmod(n, 2)
+                result = power_recursive(x, n - 1) * power_recursive(x, 1)
+                memo[n] = result
+                return memo[n]
+
+        result = power_recursive(x, n)
+        # print(calcs)
+        return round(result, 8)
 
 
 # Test cases
