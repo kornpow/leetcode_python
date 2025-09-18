5b36de5ddbe0364170978fec2656f258394079a7
Sam Korn <korn94sam@gmail.com>
2025-09-17 22:01:53 -0600

    0015: solve slowly and then solve faster with iterative rounds of assistance

--- 0015_3Sum/test_solution.py
+++ 0015_3Sum/test_solution.py
@@ -1,139 +1,120 @@
+from typing import List
+
 import pytest
-from typing import List, Optional, Type
 
-
-# Basically all this problem is saying is that we need 3 different indexes, i,j,k
-# and summing the values at those indexes==0
-
-# unless there are 3 0's, there has to be either 2 positive and 1 neg
-# OR 2 neg and 1 pos
-
-# [-1,0,1,2,-1,-4]
-# SORT THEM!
-# [-4, -1, -1, 0, 1, 2]
-# choose X1 = -4
-# left: [-1,-1]
-# middle: [0]
-# right: [1, 2]
-
-# if SUM1 < 0:
-#     choose biggest number
-
-# [-4 , 2]
-# # choose X2 = 2
-# # left: [-1,-1]
-# # middle: [0]
-# # right: [1]
-
-# SUM2 = X1 + X2
-
-# if SUM2 < 0:
-#      choose X3 = largest number
-
-# SUM1 = X1 + X2 + X3
-
-# if SUM3 < 0:
-#     # then it is impossible for X1 to ever successfully sum to 0
-#     # therefore drop it
-
-# START_LIST = [-1, -1, 0, 1, 2]
-
-# X1 = Smallest number = -1
-# SUM1 = X1
-
-# if X1 < 0
-# if SUM1 < 0:
-#     choose biggest number
-
-# [-1,0,1,2]
-# X2 = 2
-# SUM2 = X1 + X2 = -1 + 2 = 1
-
-# if SUM2 > 1:
-#     Choose smallest number
-
-# [-1,0,1]
-
-# X3 = -1
-# SUM3 = -1 + 2 + -1 = 0
-# SUCCESS! add [-1,2,-1] to final
-
-# DROP X1 again???
-
-# START_LIST = [-1, 0, 1, 2]
-
-# X1 = -1
-# SUM1 = -1
-
-# X2 = 2
-# SUM2 = -1 + 2 = 1
-
-# CHOOSE SMALLEST
-
-# X3 = 0
-# SUM3 = -1 + 2 + 0 = 1
-
-# DROP LARGEST
-
-# START_LIST = [-1, 0, 1]
+# 3SUM we need to return VALUES, in 2SUM we are return INDEXES to Values
 
 
 # Paste the LeetCode solution class here
 class Solution:
+    def threeSum_take1(self, nums: List[int]) -> List[List[int]]:
+        # print(f"3SUM:\n{nums}")
+        solution = []
+        n = len(nums)
+        # create a map which swaps value for index
+        num_map = {}
+        solution_map = {}
+        for i, val in enumerate(nums):
+            num_map[val] = i
+
+        # print(f"NUM MAP: {num_map}")
+
+        # n1 is an index
+        # O(N)
+        for n1 in range(n - 1):
+            # n2 is an index
+            # O(N)
+            for n2 in range(1, n):
+                # indexes CANT be the same
+                if n1 == n2:
+                    continue
+
+                # ht is a number
+                ht = nums[n2] + nums[n1]
+                # print(f"Sum of first two numers: {ht}")
+                ht_inverse = -ht
+                # print(f"Find the inverse number in the num_map: {ht_inverse}")
+
+                # O(1)
+                if ht_inverse in num_map:
+                    # check that it doesnt repeat one of the first two
+                    if num_map[ht_inverse] == n2 or num_map[ht_inverse] == n1:
+                        continue
+
+                    n3 = num_map[ht_inverse]
+                    # print(f"found a triplet! {n1} {n2} {n3}")
+                    # print(f"Total sum: {nums[n3] + nums[n1] + nums[n2]}")
+                    tempresult = [nums[n3], nums[n1], nums[n2]]
+                    # tempresult.sort()
+                    # result_marker = f"{n1}:{n2}:{n3}"
+                    result_marker = n1 + n2 + n3
+                    if result_marker not in solution_map:
+                        solution_map[result_marker] = 1
+                        solution.append(tempresult)
+
+        # print("A SOLUTION!")
+        # print(solution_map)
+        # print(solution)
+        return solution
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
-            return []
+        n = len(nums)
+        # print(f"3SUM: {nums}")
+        # print(nums)
+        nums.sort()
+        # print(nums)
+        results = []
+        last_num = float("inf")
+        for pivot, val in enumerate(nums):
+            if val == last_num:
+                # last_num = val
+                continue
 
-        found_list = []
+            last_num = val
 
-        start_list = sorted(nums)
+            # sorted list so if the first number is > 0 then none of the rest can be less than 0
+            if val > 0:
+                break
+            start_index = pivot + 1
+            end_index = n - 1
 
-        while len(start_list) > 3:
-            working_list = start_list[:]
-            print(f"WORKING LIST: {working_list}")
-            # choose smallest
-            X1 = working_list.pop(0)
-            SUM1 = X1
+            while start_index < end_index:
+                # reached the end
+                if start_index == n - 1:
+                    break
 
-            if SUM1 < 0:
-                X2 = working_list.pop()
-            elif SUM1 > 0:
-                X2 = working_list.pop(0)
-            else:
-                # shortcut?
-                X2 = working_list.pop()
+                result = [val, nums[start_index], nums[end_index]]
+                print(result)
+                try:
+                    sum3 = sum(result)
+                except IndexError as e:
+                    # print(f"error at : {start_index}")
+                    raise e
 
-            SUM2 = SUM1 + X2
+                # print(f"Sum of all three: {sum3}")
+                if sum3 == 0:
+                    # result.sort()
+                    # if result not in results:
+                    print(f"append: {result}")
+                    results.append(result)
 
-            if SUM2 < 0:
-                # get biggest
-                X3 = working_list.pop()
-            elif SUM2 > 0:
-                # get smallest
-                X3 = working_list.pop(0)
-            else:
-                # get biggest
-                X3 = working_list.pop()
+                    # Skip duplicates for start_index
+                    while start_index < end_index and nums[start_index] == nums[start_index + 1]:
+                        start_index += 1
+                    # Skip duplicates for end_index
+                    while start_index < end_index and nums[end_index] == nums[end_index - 1]:
+                        end_index -= 1
 
-            SUM3 = SUM2 + X3
+                    start_index += 1
+                    end_index -= 1
 
-            # modify the original list to eliminate infinite loops
-            if SUM3 < 0:
-                # get biggest
-                print("Pop Smallest")
-                start_list.pop(0)
-            elif SUM3 > 0:
-                # get smallest
-                print("Pop Biggest")
-                start_list.pop()
-            else:
-                print("Pop Smallest")
-                start_list.pop()
-                # get biggest
-                found_list.append([X1, X2, X3])
+                elif sum3 > 0:
+                    end_index -= 1
+                else:
+                    start_index += 1
 
-        # print(partials)
-        return found_list
+        print(results)
+        return results
 
 
 # Test cases
@@ -144,8 +125,17 @@ class Solution:
         ([0, 1, 1], []),
         ([0, 0, 0], [[0, 0, 0]]),
         ([-2, -4, 6, 0, -1, 1], [[-2, -4, 6], [0, -1, 1]]),
+        ([0, 0, 0, 0], [[0, 0, 0]]),
     ],
 )
 def test_solution(x, expected):
     solution = Solution()
-    assert solution.threeSum(x) == expected
+
+    result2 = solution.threeSum(x)
+    for sol in expected:
+        assert list(sorted(sol)) in list(sorted(result2))
+
+    # initial run with our brute force approach
+    # result = solution.threeSum_take1(x)
+    # for sol in expected:
+    #     assert sol in result
